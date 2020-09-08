5b670767d57594fd59fe7d3ed726fe651c8d5730
kbetlach <kbetlach@gmail.com>
2020-09-08 15:11:33 -0500

    filling in some basic css and background images to get style started

--- fantasy_cya/src/components/pages/Start.js
+++ fantasy_cya/src/components/pages/Start.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from "../Button/Button";
+import '../../App.css';
 
 function Start() {
   return (
     <div className="container">
       <div className="jumbotron">
-        <h1>Fantasy Choose Your Own Adventure!</h1>
-          <p>Click Begin to start your adventure.</p>
+        <h1 className="text-center">Fantasy Choose Your Own Adventure!</h1>
+          <h2 className="text-center">Click Begin to start your adventure.</h2>
+          
             <Link to="/character"
               className={window.location.pathname === "/character"}>
                 <Button>
                   Begin
                 </Button>
             </Link>
+            
       </div>
     </div>
   );
